75d187b512b348e0432d40b2593bfb520fd0a089
vlaship <vlaship@outlook.com>
2020-04-27 00:38:56 +0300

    added Dockerfile

--- src/main/java/vlaship/price/h2/App.java
+++ src/main/java/vlaship/price/h2/App.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class H2Application {
+public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(H2Application.class, args);
+		SpringApplication.run(App.class, args);
 	}
 
 }
